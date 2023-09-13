a48933383cfb202394410623b0c8a3093168c183
qnicole-zakka <54155870+qnicole-zakka@users.noreply.github.com>
2023-09-13 00:34:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def generate_response(input_text):
 with st.form('my_form'):
     text = st.text_area('Enter text here: ', 'Chinese law bar v.s. U.S. law bar, which test is harder')
     submitted = st.form_submit_button('Submit')
-    if not my_key.starswith("sk-"):
+    if not my_key.startswith("sk-"):
         st.warning("Please enter a valid OpenAI API Key")
     if submitted and my_key.startswith("sk-"):
         generate_response(text)
